dd40da4619f2e27778bd58eb0e1f5a1e179d9928
AFreeChameleon <ben.evans@chamel.io>
2022-03-31 00:50:05 +0100

    Added mailgun host

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -11,7 +11,8 @@ import {
 
 const mailer: Mailgun = mailgun({
     apiKey: process.env.MAILGUN_API_KEY as string,
-    domain: process.env.MAILGUN_DOMAIN as string
+    domain: process.env.MAILGUN_DOMAIN as string,
+    host: process.env.MAILGUN_HOST as string,
 });
 
 // const verifyEmailHTML = readFileSync(path.join(process.cwd(), './html/verification.html'));
@@ -29,7 +30,7 @@ export const sendPasswordChangedEmail = (userEmail: string) => {
     return new Promise((resolve, reject) => {
         let newPasswordChangedHTML = passwordChangedHTML.toString();
         const data = {
-            from: 'Support Chameleo <info@chameleo.dev>',
+            from: 'Support Chameleo <panther@chameleo.dev>',
             to: userEmail,
             subject: 'Your password has been changed',
             html: newPasswordChangedHTML
@@ -57,7 +58,7 @@ export const sendDeleteAccountEmail = (userEmail: string, token: string) => {
         let newDeleteAccountHTML = deleteAccountHTML.toString()
             .replace('TOKEN_URL', `${process.env.HOST}/delete-account/${token}`);
         const data = {
-            from: 'Support Chameleo <info@chameleo.dev>',
+            from: 'Support Chameleo <panther@chameleo.dev>',
             to: userEmail,
             subject: 'Delete your account',
             html: newDeleteAccountHTML
@@ -85,12 +86,13 @@ export const sendVerifyEmail = (userEmail: string, token: string) => {
         let newVerifyEmailHTML = verifyEmailHTML.toString()
             .replace('TOKEN_URL', `${process.env.HOST}/verify-email/${token}`);
         const data = {
-            from: 'Support Chameleo <info@chameleo.dev>',
+            from: 'Support Chameleo <panther@chameleo.dev>',
             to: userEmail,
             subject: 'Verify your email!',
             html: newVerifyEmailHTML
         };
-        mailer.messages().send(data, (err, body) => {
+        mailer.messages().send(data, (err, body, ...args) => {
+            console.log(args, process.env.MAILGUN_API_KEY, process.env.MAILGUN_DOMAIN)
             if (err) {
                 console.log('err', err)
                 reject({
@@ -98,7 +100,7 @@ export const sendVerifyEmail = (userEmail: string, token: string) => {
                     message: err
                 });
             } else {
-                console.log('body', body)
+                console.log('body', body, data)
                 resolve({
                     error: false,
                     message: body
@@ -112,7 +114,7 @@ export const sendChangedEmailAddressEmail = (userEmail: string) => {
     return new Promise((resolve, reject) => {
         const newResetPassHTML = changedEmailHTML.toString();
         const data = {
-            from: 'Support Chameleo <info@chameleo.dev>',
+            from: 'Support Chameleo <panther@chameleo.dev>',
             to: userEmail,
             subject: 'Successfully changed your email.',
             html: newResetPassHTML
@@ -138,7 +140,7 @@ export const sendResetPassEmail = (userEmail: string, token: string) => {
         const newResetPassHTML = resetPassHTML.toString()
             .replace('TOKEN_URL', `${process.env.HOST}/forgot-password/${token}`);
         const data = {
-            from: 'Support Chameleo <info@chameleo.dev>',
+            from: 'Support Chameleo <panther@chameleo.dev>',
             to: userEmail,
             subject: 'Reset your password!',
             html: newResetPassHTML
